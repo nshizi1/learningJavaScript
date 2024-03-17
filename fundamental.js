4228cf7a91d3b70f8988ec66d517e1c174bbbf88
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-17 16:04:06 +0200

    development

---
+++ fundamental.js
@@ -0,0 +1,13 @@
+var number;
+number = 4;
+console.log(number);
+if(number != 4){
+    console.log("number is not 4");
+}else{
+    console.log("number is 4");
+}
+if(number <= 0){
+    console.log("number is less than 0");
+}else{
+    console.log("number is greater than 0");
+}   
